4e2e25d379267c8874f7ebab2e228b36e3c2c4b3
kx <kx@gmail.com>
2023-05-15 15:38:45 +0800

    test

---
+++ pipe.tf
@@ -0,0 +1,28 @@
+
+module "codepipeline" {
+  source = "github.com/rpstreef/terraform-aws-codepipeline?ref=v1.0"
+
+  resource_tag_name = var.resource_tag_name
+  environment       = var.environment
+  region            = var.region
+
+  codepipeline_module_enabled = var.codepipeline_module_enabled
+
+  git_owner        = var.git_owner
+  git_repo         = var.git_repo
+  git_branch       = var.git_branch
+
+
+  environment_variable_map = [
+    {
+      name  = "DOMAIN"
+      value = var.domain_name
+      type  = "PLAINTEXT"
+    },
+    {
+      name  = "CACHE"
+      value = var.domain_cache_settings
+      type  = "PLAINTEXT"
+    }
+  ]
+}
