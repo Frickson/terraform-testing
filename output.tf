1d61f2e240404d28c6f18a0191c3deaedb953164
kx <kx@gmail.com>
2023-05-25 11:22:15 +0800

    test

--- output.tf
+++ output.tf
@@ -1,8 +1,12 @@
-output test{
+output "test" {
     value = local.aws_root_account_arn
 }
 
 output "example" {
     value = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["chatbot_github_token"]
     sensitive = true
+}
+
+output "arm" {
+    value = module.build.role_id
 }
