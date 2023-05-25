cf8c887002fe1e8dd373548b169c62e837c52d52
kx <kx@gmail.com>
2023-05-25 12:07:22 +0800

    test

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ data "aws_iam_policy_document" "assume_role_eks" {
 }
 
 resource "aws_iam_policy" "assume_role_eks_policy" {
-  name        = "sts_assume_eks_deploy_role_${module.this.id}"
+  name        = "sts_assume_eks_deploy_role"
   description = "A policy to assume role given by specific account to deploy resouces on their account"
   policy      = data.aws_iam_policy_document.assume_role_eks.json
 }
