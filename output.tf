a8527c34b8b1b1500da367e9583963d64c5f603a
kx <kx@gmail.com>
2023-05-26 15:30:00 +0800

    test

--- output.tf
+++ output.tf
@@ -11,14 +11,8 @@ output "role_arn" {
   description = "IAM Role ARN"
   value = [for index in module.codebuild:{
     arn = index.role_arn
+    project_id = index.project_id
   }]
 }
 
-
-output "project_id" {
-  /* description = "IAM Role ARN" */
-  value = [for index in module.codebuild:{
-    arn = index.project_id
-  }]
-}
 
