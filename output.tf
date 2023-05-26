1442f7d1f0f8f394b65ac9c943afab75aca17946
kx <kx@gmail.com>
2023-05-26 16:16:40 +0800

    test

--- output.tf
+++ output.tf
@@ -23,5 +23,5 @@ output "project_id" {
 }
 
 output "testabc" {
-  value = length(local.test) ? [for x in local.test: x.project_id if x.name == "build3"][0] : "shit"
+  value = length(local.test)!=0 ?[for x in local.test: x.project_id if x.name == "build3"][0]:"shit"
 }
