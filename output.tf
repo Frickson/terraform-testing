ea91709ccfa8bfbf1e6c817ba26d14de4ef889a9
kx <kx@gmail.com>
2023-05-26 16:06:10 +0800

    test

--- output.tf
+++ output.tf
@@ -23,5 +23,5 @@ output "project_id" {
 }
 
 output "testabc" {
-  value = [for x in local.test: x.project_id if x.name == "build3"]
+  value = [for x in local.test: x.project_id if x.name == "build3"][0]
 }
