8d63c97e5f5af6c3d349b6ec4a64ddd8987d5477
kx <kx@gmail.com>
2023-05-29 14:42:12 +0800

    test

--- lambda.tf
+++ lambda.tf
@@ -9,22 +9,23 @@ module "lambda_function" {
   function_url_auth_type = each.value.function_url_auth_type
 }
 
-locals {
+/* locals { */
   /* updated_function_1 = merge(
     var.lambda["function-1"],
     {
       new_attribute = "value"
     }
   ) */
-  new_value = [for x in var.lambda: merge(x,
+  /* new_value = [for x in var.lambda: merge(x,
     {
       new_attribute = "value"
-    })]
-}
+    })] */
+  
+/* } */
 
-output "updated_function_1" {
+/* output "updated_function_1" {
   value = local.new_value
-}
+} */
 /* output "function_name"{
     value = module.lambda.function_name
 }
@@ -47,4 +48,26 @@ resource "aws_lambda_function_url" "function_url" {
     expose_headers    = ["keep-alive", "date"]
     max_age           = 86400
   }
+} */
+
+/* local {
+  rds = {
+    "22" = ["192.168.173.80", "172.80.30.10"]
+    "3306" = ["0.0.0.0/0"]
+  }
+  eks = {
+  }
+}
+resource "aws_security_group" "example" {
+  name = "test"
+}
+
+resource "aws_security_group_rule" "example" {
+  for_each = var.rules
+  type              = "ingress"
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  protocol          = each.value.protocol
+  cidr_blocks       = local.test[each.value.to_port] ? local.test[each.value.to_port] : []
+  security_group_id = aws.security_group.example.id
 } */
--- variable.tf
+++ variable.tf
@@ -153,3 +153,4 @@ variable "lambda" {
     }
   }
 }
+
