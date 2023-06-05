dd07d6dae3295b1c206f908c3fb07fa8b77518c3
kx <kx@gmail.com>
2023-06-05 17:24:21 +0800

    test

--- codebuild-chatbot/variable.tf
+++ codebuild-chatbot/variable.tf
@@ -1,10 +1,10 @@
 locals {
   codebuild = {
-    "buildspec-build_image" = {
-      name          = "buildspec-build_image"
+    "build-image" = {
+      name          = "build-image"
       source_type   = "GITHUB"
       source_location = var.source_location_url
-      buildspec     = "chatbot/buildspecs/buildspec-build_image.yml"
+      buildspec     = "chatbot/buildspecs/buildspec-build-image.yml"
       privileged_mode = true
       cache_type    = "LOCAL"
       local_cache_modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
