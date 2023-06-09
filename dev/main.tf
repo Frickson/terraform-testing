775f7d9a1e717027b01612f2869b0443490b0e92
kx <kx@gmail.com>
2023-06-09 11:15:14 +0800

    test

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,5 @@
 module "dev_chatbot" {
-  source = "github.com/nec-msbu-devops/aws-chatbot-module?ref=v1.0"
+  source = "github.com/nec-msbu-devops/aws-chatbot-module"
   environment = var.environment
   git_branch = var.git_branch
   eks_role = var.eks_role
