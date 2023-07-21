ebd3ec1f3add2956e79e91445eba271ec163333b
kx <kx@gmail.com>
2023-07-21 14:23:50 +0800

    test

--- stag/output.tf
+++ stag/output.tf
@@ -1,8 +1,8 @@
 output "repo_urls" {
-  value = module.dev_chatbot.repo_urls
+  value = module.stag_chatbot.repo_urls
   sensitive = true
 }
 
 output "project_names" {
-  value = module.dev_chatbot.project_names
+  value = module.stag_chatbot.project_names
 }
